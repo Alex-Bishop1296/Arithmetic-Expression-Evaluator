c5c54387226c9828844924ebf6d3f537cd44083c
Alex Bishop <Alex-Bishop1296@users.noreply.github.com>
2021-03-16 10:18:15 -0700

    Add project files.

---
+++ ArithmeticExpressionEvaluator/ArithmeticExpressionEvaluator.cpp
@@ -0,0 +1,38 @@
+//  ArithmeticExpressionEvaluator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*  Date: 3/16/2021
+    
+    Programmer: Alex Bishop
+    
+    Purpose: Interview Technical Question
+    
+    Prompt: Write a program to parse and evaluate arithmetic expressions.
+            Your problem should support addition and multiplication at a minimum.
+            You input will be text similar to the following:
+
+            1 + 1
+
+            (3 + 4) * 6
+
+            (1 * 4) + (5 * 2)
+
+            Your output should be the computed value of the expression.
+            Write your program in an Object Oriented fashion and be prepared to show us your code and discuss your solution. Do not use the "Shunting Yard" algorithm.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
